664f053093f69ea61518b9a03ffb12d365eded31
VINIT1TANDEL <155880237+VINIT1TANDEL@users.noreply.github.com>
2025-05-21 02:03:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ process.on("uncaughtException", (err) => {
 });
 
 // Load environment variables
-dotenv.config({ path: "backend/config/config.env" });
+dotenv.config({ path: "/config/config.env" });
 
 // Enable CORS
 app.use(cors({
